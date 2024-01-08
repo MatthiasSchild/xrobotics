6b16bdef386046166409074be15cec27817498b8
Matthias Schild <matthias@matthiasschild.de>
2024-01-08 08:48:07 +0000

    Updated firmware with led and servo control.

---
+++ alpha/firmware/include/pins.h
@@ -0,0 +1,2 @@
+#define LED_YELLOW 16
+#define LED_BLUE 5
--- alpha/firmware/src/main.cpp
+++ alpha/firmware/src/main.cpp
@@ -1,10 +1,10 @@
 #define DEBUG_WIFI 1
 
 #include <Arduino.h>
-
 #include <ESP8266WiFi.h>
 #include <ESP8266WebServer.h>
-
+#include <Servo.h>
+#include "pins.h"
 #define HTTP_REST_PORT 80
 
 ESP8266WebServer server(HTTP_REST_PORT);
@@ -18,55 +18,98 @@ IPAddress subnet(255, 255, 255, 0);
 String ssid = "XRobotics";
 String pw = "projectalpha";
 
-void getHelloWord() 
+Servo servo1;
+
+void getHelloWord()
 {
-    server.send(200, "text/json", "{\"name\": \"Hello world\"}");
+  server.send(200, "text/json", "{\"name\": \"Hello world\"}");
+}
+void handleLed1On()
+{
+  digitalWrite(LED_YELLOW, HIGH);
+  server.send(200, "text/plain", "ok");
+}
+void handleLed1Off()
+{
+  digitalWrite(LED_YELLOW, LOW);
+  server.send(200, "text/plain", "ok");
+}
+void handleLed2On()
+{
+  digitalWrite(LED_BLUE, HIGH);
+  server.send(200, "text/plain", "ok");
+}
+void handleLed2Off()
+{
+  digitalWrite(LED_BLUE, LOW);
+  server.send(200, "text/plain", "ok");
+}
+void handleServo()
+{ // Handler for the body path
+
+  if (server.hasArg("plain") == false)
+  { // Check if body received
+
+    server.send(200, "text/plain", "Body not received");
+    return;
+  }
+  String message = server.arg("plain");
+  int servoAngle = message.toInt();
+  servo1.write(servoAngle);
+  // Serial.println(message);
+  server.send(200, "text/plain", "ok");
 }
 
-void restServerRouting() 
+void restServerRouting()
 {
-    server.on("/", HTTP_GET, []() 
-    {
-      server.send(200, F("text/html"),F("Welcome to the REST Web Server"));
-    });
-    server.on(F("/helloWorld"), HTTP_GET, getHelloWord);
+  server.on("/", HTTP_GET, []()
+            { server.send(200, F("text/html"), F("Welcome to the REST Web Server")); });
+  server.on(F("/helloWorld"), HTTP_GET, getHelloWord);
+  server.on(F("/led1/on"), HTTP_GET, handleLed1On);
+  server.on(F("/led1/off"), HTTP_GET, handleLed1Off);
+  server.on(F("/led2/on"), HTTP_GET, handleLed2On);
+  server.on(F("/led2/off"), HTTP_GET, handleLed2Off);
+  server.on(F("/servo"), HTTP_POST, handleServo);
 }
-
-bool setupWifi() {
+bool setupWifi()
+{
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(ip, ip_zero, subnet);
   return WiFi.softAP("XRobotics", "projectalpha");
 }
 
-void setupSerial() {
-
-}
-
-void setupServer() {
-    ready = true;
-    Serial.println("Ready");
-    delay(1000);
-    restServerRouting();
-
-    server.begin();
-    Serial.println(WiFi.softAPIP().toString());
-}
-
-void setup() 
+void setupSerial()
 {
-  // pinMode(16, OUTPUT);
-  // pinMode(5, OUTPUT);
+}
+
+void setupServer()
+{
+  ready = true;
+  Serial.println("Ready");
+  delay(1000);
+  restServerRouting();
+
+  server.begin();
+  Serial.println(WiFi.softAPIP().toString());
+}
+
+void setup()
+{
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
+  pinMode(4, OUTPUT);
+  servo1.attach(4);
 
   Serial.begin(9600);
   Serial.println();
 
-  //Wifi setup
+  // Wifi setup
   Serial.print("Setting soft-AP ... ");
 
   WiFi.mode(WIFI_AP);
   WiFi.softAPConfig(ip, ip_zero, subnet);
   boolean result = setupWifi();
-  if(result == true)
+  if (result == true)
   {
     setupServer();
   }
@@ -76,9 +119,9 @@ void setup()
   }
 }
 
-void loop() 
+void loop()
 {
-  if (ready) 
+  if (ready)
   {
     server.handleClient();
   }
@@ -87,10 +130,10 @@ void loop()
   // Serial.println(input);
   // input.trim();
   // if (input == "on") {
-  //   digitalWrite(16, HIGH);
-  //   digitalWrite(5, HIGH);
+  //   digitalWrite(LED_YELLOW, HIGH);
+  //   digitalWrite(LED_BLUE, HIGH);
   // } else if (input == "off") {
-  //   digitalWrite(16, LOW);
-  //   digitalWrite(5, LOW);
+  //   digitalWrite(LED_YELLOW, LOW);
+  //   digitalWrite(LED_BLUE, LOW);
   // }
 }
